3a9cbad40d4b453a01c8f99158bcf21560e55601
machadothi <thimachadoo@gmail.com>
2018-05-05 16:58:17 -0300

    first commit

---
+++ ADC/project0/Debug/project0.d
@@ -0,0 +1,23 @@
+# FIXED
+
+project0.obj: ../project0.c
+project0.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+project0.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+project0.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+
+../project0.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
